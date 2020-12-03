5a5d25f1c78eb97f6889f90301b5a69e87b5cbbc
Tiger Nie <nhl0819@gmail.com>
2020-12-03 13:58:40 +0800

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -262,6 +262,12 @@ const themes = {
     icon_color: "a64833",
     text_color: "d9c8a9",
     bg_color: "402b23"
+  },
+  halcyon: {
+    title_color: "5ccfe6",
+    icon_color: "bae67e",
+    text_color: "d7dce2",
+    bg_color: "171c28"
   }
 };
 
